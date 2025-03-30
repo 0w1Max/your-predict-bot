3d8cf6ad9a882f3cf7c805dce2e790708e2150da
Ow1Max <0w1.go.0w1@gmail.com>
2025-03-30 23:53:00 +0300

    fix: update db.js

--- src/db.js
+++ src/db.js
@@ -3,11 +3,8 @@ require('dotenv').config();
 const logger = require('./logger');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on('connect', () => {
@@ -16,28 +13,6 @@ pool.on('connect', () => {
 
 pool.on('error', (err) => {
   logger.error('❌ Ошибка подключения к PostgreSQL:', err);
-});
-
-(async () => {
-  try {
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS predictions (
-        id SERIAL PRIMARY KEY,
-        text TEXT NOT NULL
-      );
-    `);
-    logger.info('✅ Таблица predictions проверена/создана.');
-  } catch (err) {
-    logger.error('❌ Ошибка создания таблицы:', err);
-  }
-})();
-
-// Закрытие пула при завершении работы
-process.on('SIGINT', async () => {
-  logger.info('⏳ Завершение работы, закрытие соединения с БД...');
-  await pool.end();
-  logger.info('✅ Соединение с PostgreSQL закрыто.');
-  process.exit(0);
 });
 
 module.exports = pool;
